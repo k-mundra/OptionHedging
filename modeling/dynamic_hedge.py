6edd90265980accdc794d849d3f5b6fa7d9ffe44
Soumil Sahu <soumil01@gmail.com>
2025-10-22 15:29:11 -0400

    simplify heston path simulator and expand code comments

--- modeling/dynamic_hedge.py
+++ modeling/dynamic_hedge.py
@@ -1,9 +1,34 @@
-def dynamic_hedge(S0, v0, r, kappa, theta, sigma, rho, T, N, M, h, K):
-    pass
-    '''hedge a portfolio short 1 option and long delta stock, with any cash earning interest at r'''
+from heston_model import sim_heston_paths, heston_greeks, heston_option_price
+import numpy as np
+import matplotlib.pyplot as plt
 
+def dynamic_hedge(hedge_freq = 10):
+    pass
+
+    S0 = 100 # initial value of stock price
+    v0 = 0.04 # initial value of variance
+    r = 0.05 # risk free rate
+    kappa = 2.0 # rate of mean inversion of variance
+    theta = 0.04 # long term mean of variance
+    sigma = 0.3 # volatility of volatility
+    rho = -0.7 # correlation between Weiner processes
+    T = 1.0 # maturity in years
+    K = 110 # strike price
+    num_steps_outer = 365 # steps
+    num_paths_outer = 10000 # paths for outer (baseline) simulation
+    h = 1e-3 # perturbation to stock price for greek computation
+
+    num_paths_inner = 
+
+
+    '''
+    input to function: hedge_freq -> this is the hedging frequency in days,
+    use this to figure out what num_steps to use as inputs sim_heston_paths and how often we call it
+    '''
+
+    '''TASK:hedge a portfolio short 1 option and long delta stock, with any cash earning interest at r'''
     '''implement this function by 
-    1. running MC sims at every time step
+    1. running MC sims at every time step we want to hedge at (using num_paths_inner)
     2. finding the option prices discounted to that time and corresponding greeks
     3. update portfolio
     
